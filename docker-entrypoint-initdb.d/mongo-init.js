e8d361426b9272bdd36b8e35e9aeb7122dcd855f
Mathew May <29102292+mghmay@users.noreply.github.com>
2023-05-21 12:15:46 +0100

    update mongo-init

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -12,3 +12,21 @@ db.createUser({
 });
 
 db.createCollection("users");
+
+db.users.insertMany([
+	{
+		username: "Fred",
+		email: "fred@test.com",
+		password: "password1",
+	},
+	{
+		username: "Daphne",
+		email: "daphne@test.com",
+		password: "password2",
+	},
+	{
+		username: "Scooby",
+		email: "scooby@test.com",
+		password: "password3",
+	},
+]);
