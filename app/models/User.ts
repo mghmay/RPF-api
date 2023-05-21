2ba30b4be8676dd586ee4b342f1ecdb1008b2563
Mathew May <29102292+mghmay@users.noreply.github.com>
2023-05-21 12:15:39 +0100

    Add model

---
+++ app/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose from "npm:mongoose";
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		lowercase: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+export const User = mongoose.model("User", userSchema);
