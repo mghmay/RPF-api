5343ab254059746c05d1e7ad543ab946be5b67b9
Mathew May <29102292+mghmay@users.noreply.github.com>
2023-05-21 11:55:23 +0100

    Add try catch to routes

--- app/controllers/userController.ts
+++ app/controllers/userController.ts
@@ -1,14 +1,35 @@
 export default {
-	getUsers: (ctx) => {
-		console.log("get all users!");
+	getUsers: (ctx: {request: any; response: any}) => {
+		try {
+			ctx.response.body = "getUsers";
+			ctx.request.body;
+		} catch (e) {
+			ctx.response.status = e.status;
+			ctx.response.body = e.message;
+		}
 	},
-	getUserById: (ctx) => {
-		console.log("user by id");
+	getUserById: (ctx: {response: any}) => {
+		try {
+			ctx.response.body = "getUserById";
+		} catch (e) {
+			ctx.response.status = e.status;
+			ctx.response.body = e.message;
+		}
 	},
-	createUser: (ctx) => {
-		console.log("post user");
+	createUser: async (ctx: {response: any}) => {
+		try {
+			ctx.response.body = "createUser";
+		} catch (e) {
+			ctx.response.status = e.status;
+			ctx.response.body = e.message;
+		}
 	},
-	deleteUser: (ctx) => {
-		console.log("delete user");
+	deleteUser: (ctx: {response: any}) => {
+		try {
+			ctx.response.body = "deleteUser";
+		} catch (e) {
+			ctx.response.status = e.status;
+			ctx.response.body = e.message;
+		}
 	},
 };
