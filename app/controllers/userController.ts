125ec23cd1485f76501a34c09c578597694005e7
Mathew May <29102292+mghmay@users.noreply.github.com>
2023-05-21 11:41:08 +0100

    Add routes and controller

---
+++ app/controllers/userController.ts
@@ -0,0 +1,14 @@
+export default {
+	getUsers: (ctx) => {
+		console.log("get all users!");
+	},
+	getUserById: (ctx) => {
+		console.log("user by id");
+	},
+	createUser: (ctx) => {
+		console.log("post user");
+	},
+	deleteUser: (ctx) => {
+		console.log("delete user");
+	},
+};
--- app/routes/userRouter.ts
+++ app/routes/userRouter.ts
@@ -1,7 +1,10 @@
 import {Router} from "https://deno.land/x/oak@v12.4.0/router.ts";
+import userController from "../controllers/userController.ts";
 
-export const userRouter = new Router();
+export const userRouter = new Router({prefix: "/user"});
 
-userRouter.get("/", (ctx) => {
-	console.log("Hello world!");
-});
+userRouter
+	.get("/", userController.getUsers)
+	.get("/:id", userController.getUserById)
+	.post("/", userController.createUser)
+	.delete("/:id", userController.deleteUser);
