b9fb64520058d25c73d03c47930e0b5b54c452b9
Mathew May <29102292+mghmay@users.noreply.github.com>
2023-05-22 10:55:48 +0100

    Add listening on port notification

--- app/server.ts
+++ app/server.ts
@@ -9,4 +9,12 @@ const app = new Application();
 app.use(userRouter.routes());
 app.use(userRouter.allowedMethods());
 
-app.listen({port: 3000});
+const port = 3000;
+
+app.addEventListener("listen", ({secure, port}) => {
+	const protocol = secure ? "https://" : "http://";
+	const url = `${protocol}${Deno.env.get("HOST") ?? "localhost"}:${port}`;
+	console.log(`Listening on: ${url}`);
+});
+
+await app.listen({port: port});
