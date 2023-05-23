ff789df460346ffebde2246f38404cf007d5223c
Mathew May <29102292+mghmay@users.noreply.github.com>
2023-05-23 15:53:49 +0100

    Add a base route

--- app/routes/router.ts
+++ app/routes/router.ts
@@ -3,4 +3,7 @@ import {Router} from "https://deno.land/x/oak@v12.4.0/mod.ts";
 
 export const router = new Router();
 
+router.get("/", (ctx) => {
+	ctx.response.body = "Welcome to the RPF-api";
+});
 router.use("/users", userRouter.routes());
